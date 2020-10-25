71e269c2d435365fc901a96a70f009024246a283
ferrero-manuel <49575509+ferrero-manuel@users.noreply.github.com>
2020-10-25 02:45:44 -0300

    Update vertex-shader.glsl

--- Entregas/03_demoPlanetaTierra/shaders/vertex-shader.glsl
+++ Entregas/03_demoPlanetaTierra/shaders/vertex-shader.glsl
@@ -44,7 +44,7 @@
             //float a = position.y*2.0;
             //position.x = cos(a)*position.x+sin(a)*position.z;
             //position.z = -sin(a)*position.x+cos(a)*position.z;
-            position.x = arccos(position.x);
+            position.x = acos(position.x);
 
             // ************************************************************************
 
