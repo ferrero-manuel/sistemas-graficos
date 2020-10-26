62757347e70466b88385ed866c2cae2b5a128b33
ferrero-manuel <49575509+ferrero-manuel@users.noreply.github.com>
2020-10-26 00:19:34 -0300

    Update vertex-shader-1.glsl

--- Entregas/03_demoPlanetaTierra/shaders/vertex-shader-1.glsl
+++ Entregas/03_demoPlanetaTierra/shaders/vertex-shader-1.glsl
@@ -42,8 +42,9 @@
             // **************** EDITAR A PARTIR DE AQUI *******************************
             
             // position+=normal*(1.0+sin(position.z*18.0*PI+time*20.0))*0.03;
-            position.x *= 2.0*position.x;
-
+            position.x = sin(position.x*PI/2.0);
+            position.z = sin(position.z*PI/2.0);
+            
             // ************************************************************************
 
             vec4 worldPos = uMMatrix*vec4(position, 1.0);                        
